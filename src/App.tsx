2ec12565a4ed3a6f3bfa5fb98e9ecddb43a3be47
jesvin-biju <jesvinbiju2005@gmailc.com>
2025-04-12 22:19:16 +0530

    Changes:Clerk authentication & Routing setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {PublicLayout} from "@/layouts/public-layout";
+import HomePage from "@/routes/home";
+import AuthenticationLayout from "@/layouts/auth-layout";
+import ProtectRoutes from "@/layouts/protected-routes";
 
+import { SignInPage } from "./routes/sign-in";
+import { SignUpPage } from "./routes/sign-up";
+
+import { MainLayout } from "@/layouts/main-layout";
 const App = () => {
   return (
-    <div className='text-blue-500 '></div>
+    <Router>
+      <Routes>
+        {/*public routes*/}
+        <Route element={<PublicLayout/>}>
+        <Route index element={<HomePage />} />
+        </Route>
+        {/* authentication layout */}
+        <Route element={<AuthenticationLayout />}>
+          <Route path="/signin/*" element={<SignInPage />} />
+          <Route path="/signup/*" element={<SignUpPage />} />
+        </Route>
+        {/*protected routes*/}
+        <Route
+          element={
+            <ProtectRoutes>
+              <MainLayout />
+            </ProtectRoutes>
+          }
+        >
+          {/* add all the protect routes */}
+        </Route>
+      </Routes>
+    </Router>
     
   )
 }
